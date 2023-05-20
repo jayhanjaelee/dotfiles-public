6d049ed32839b3079b61f03f44b74aa5fdae071e
Hanjae Lee <jayhanjaelee@gmail.com>
2023-05-20 22:49:40 +0900

    delete: separators.

--- .config/nvim/after/plugin/lualine.rc.lua
+++ .config/nvim/after/plugin/lualine.rc.lua
@@ -5,8 +5,8 @@ lualine.setup {
   options = {
     icons_enabled = true,
     theme = 'solarized_dark',
-    section_separators = { left = '', right = '' },
-    component_separators = { left = '', right = '' },
+    section_separators = '',
+    component_separators = '',
     disabled_filetypes = { 'neo-tree' }
   },
   sections = {
