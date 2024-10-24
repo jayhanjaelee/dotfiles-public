e9cb6874d63988e14a0964605a76331fadf173e9
Hanjae Lee <jayhanjaelee@gmail.com>
2024-10-24 15:22:23 +0900

    Add powershell.

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,14 @@
+function prompt {
+  $loc = $executionContext.SessionState.Path.CurrentLocation;
+
+$out = ""
+  if ($loc.Provider.Name -eq "FileSystem") {
+    $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+  }
+  $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
+  return $out
+}
+
+Set-Alias vim nvim
+Set-Alias vi nvim
+Set-Alias v nvim
